498c4273bd3b0e647d220baf04784e4f5918d743
Aaron Samir Padilla Castillo <aapadill@c3r4p3.hive.fi>
2025-03-17 12:59:02 +0200

    hands relative to windows size

--- src/hands.c
+++ src/hands.c
@@ -14,19 +14,28 @@
 
 void draw_hud_hands(t_data *data)
 {
-	mlx_texture_t *hand_tex = data->hud_hands[data->hud_frame];
+	mlx_texture_t	*hand_tex = data->hud_hands[data->hud_frame];
+
+	float			target_height = data->height * 0.6f;
+	float			scale_factor = target_height / hand_tex->height;
+
+	int				new_width = hand_tex->width * scale_factor;
+	int				new_height = hand_tex->height * scale_factor;
 
 	// bottom center of the screen
-	int pos_x = (data->width - hand_tex->width) / 2;
-	int pos_y = data->height - hand_tex->height;
+	int pos_x = (data->width - new_width) / 2;
+	int pos_y = data->height - new_height;
 
-	uint32_t y = 0;
-	while (y < hand_tex->height)
+	int y = 0;
+	while (y < new_height)
 	{
-		uint32_t x = 0;
-		while (x < hand_tex->width)
+		int x = 0;
+		while (x < new_width)
 		{
-			uint32_t color = get_texture_color(hand_tex, x, y);
+			int	src_x = x / scale_factor;
+			int	src_y = y / scale_factor;
+
+			uint32_t color = get_texture_color(hand_tex, src_x, src_y);
 			color = simple_shading(color, 1.0);
 			// Skip fully transparent pixels (assumes transparency is indicated in the lower 24 bits)
 			if ((color & 0xFF000000) != 0)
@@ -38,4 +47,3 @@ void draw_hud_hands(t_data *data)
 		y++;
 	}
 }
-
