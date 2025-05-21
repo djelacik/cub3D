87df33d0ce5aad1c1e7a80129ff9a9d5e75d1f28
Daniel Jelacik <143644426+djelacik@users.noreply.github.com>
2025-05-21 14:58:55 +0300

    norm fixes

--- src/draw_floor_ceiling.c
+++ src/draw_floor_ceiling.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_floor_ceiling.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: djelacik <djelacik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 00:35:17 by aapadill          #+#    #+#             */
-/*   Updated: 2025/05/08 14:53:19 by aapadill         ###   ########.fr       */
+/*   Updated: 2025/05/21 14:58:33 by djelacik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ void	draw_floor_and_ceiling(t_data *data)
 		while (x < data->width)
 		{
 			if (y < data->height / 2)
-				mlx_put_pixel(data->image, x, y, data->ceiling); // Katon väri
+				mlx_put_pixel(data->image, x, y, data->ceiling);
 			else
-				mlx_put_pixel(data->image, x, y, data->floor); // Lattian väri
+				mlx_put_pixel(data->image, x, y, data->floor);
 			x++;
 		}
 		y++;
